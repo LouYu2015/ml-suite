db233e5c55b2937803bde97993d082bc3080c707
Tom Lou <louyu27@cs.washington.edu>
2020-05-25 15:20:09 -0700

    Add arguments

--- examples/gRPC/naive_client.py
+++ examples/gRPC/naive_client.py
@@ -7,21 +7,32 @@ import grpc
 import numpy as np
 import time
 
-# gRPC server info
-
-SERVER_ADDRESS = "localhost"
-SERVER_PORT = 5000
-
 # Number of dummy images to send
 N_DUMMY_IMAGES = 1000
 
 INPUT_NODE_NAME = "data"
 OUTPUT_NODE_NAME = "fc1000/Reshape_output"
 
-BATCH_SIZE = 4
+STACK = True
 
 IMAGE_LIST = "~/CK-TOOLS/dataset-imagenet-ilsvrc2012-aux/val.txt"
 IMAGE_DIR = "~/CK-TOOLS/dataset-imagenet-ilsvrc2012-val-min"
+
+import argparse
+parser = argparse.ArgumentParser(description='Xilin ML Suit gRPC Client')
+parser.add_argument('--batchsize', metavar="<batch size>", type=int,
+                    help="Batch size", default=10)
+parser.add_argument('-n', type=int, help="Number of images", default=490)
+parser.add_argument("--address", metavar="<address>", type=str,
+                    help="Server address", default="localhost")
+parser.add_argument("-p", metavar="<port>", type=int,
+                    help="Server port", default=5000)
+args = parser.parse_args()
+
+N_IMAGENET_IMAGES = args.n
+BATCH_SIZE = args.batchsize
+SERVER_ADDRESS = args.address
+SERVER_PORT = args.p
 
 
 

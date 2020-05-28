360e4b2a80a7b85b9030945efbac2efe44269a6d
Tom Lou <louyu27@cs.washington.edu>
2020-05-28 16:23:11 -0700

    Implemented proxy

---
+++ examples/gRPC/grpc_proxy.py
@@ -0,0 +1,83 @@
+import protos.grpc_service_pb2_grpc as grpc_service_pb2_grpc
+import protos.server_status_pb2 as server_status_pb2
+import protos.model_config_pb2 as model_config_pb2
+import protos.request_status_pb2 as request_status_pb2
+import grpc
+import multiprocessing as mp
+from concurrent import futures
+
+import itertools
+
+ADDRESSES = ["127.0.0.1:{port}".format(port=port) for port in range(5001, 5008 + 1)]
+PORT = 5000
+
+
+def iterator_split(iterator, size):
+    iterators = itertools.tee(iterator, size)
+    result = []
+    for i, it in enumerate(iterators):
+        result.append(itertools.islice(it, i, None, size))
+    return result
+
+
+def roundrobin(*iterables):
+    "roundrobin('ABC', 'D', 'EF') --> A D E B F C"
+    # Recipe credited to George Sakkis
+    pending = len(iterables)
+    nexts = itertools.cycle(iter(it).next for it in iterables)
+    while pending:
+        try:
+            for next in nexts:
+                yield next()
+        except StopIteration:
+            pending -= 1
+            nexts = itertools.cycle(itertools.islice(nexts, pending))
+
+
+class InferenceServicer(grpc_service_pb2_grpc.GRPCServiceServicer):
+    '''
+    A proxy server that distributes requests
+    '''
+    def __init__(self, addresses):
+        self.addresses = addresses
+        self.next_address = 0
+
+    def Status(self, request, context):
+        master = self.addresses[0]
+        with grpc.insecure_channel(master) as channel:
+            stub = grpc_service_pb2_grpc.GRPCServiceStub(channel)
+            return stub.Status(request)
+
+    def Infer(self, request, context):
+        master = self.addresses[self.next_address % self.addresses(len)]
+        self.next_address += 1
+        if self.next_address > 1000000:
+            self.next_address = 0
+        with grpc.insecure_channel(master) as channel:
+            stub = grpc_service_pb2_grpc.GRPCServiceStub(channel)
+            return stub.Infer(request)
+
+    def StreamInfer(self, request_iterator, context):
+        # request iterators
+        request_its = iterator_split(request_iterator, len(self.addresses))
+        channels = [grpc.insecure_channel(address)
+                    for address in self.addresses]
+        try:
+            results = [grpc_service_pb2_grpc.GRPCServiceStub(channel).StreamInfer(it)
+                       for channel, it in zip(channels, request_its)]
+            return roundrobin(results)
+        finally:
+            for channel in channels:
+                channel.close()
+
+def main():
+    executor = futures.ThreadPoolExecutor(max_workers=GRPC_WORKER_COUNT)
+    executor.shutdown = lambda wait: None
+    server = grpc.server(executor,
+                         options=(('grpc.max_receive_message_length', 64*1024*1024),))
+    servicer = InferenceServicer(ADDRESSES)
+    grpc_service_pb2_grpc.add_GRPCServiceServicer_to_server(servicer,
+                                                              server)
+
+    # Bind port
+    server.add_insecure_port('[::]:{port}'.format(port=PORT))

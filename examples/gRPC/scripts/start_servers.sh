f9526e25658bda4d635061668f14b87f5b0789db
Tom Lou <louyu27@cs.washington.edu>
2020-05-28 17:52:29 -0700

    Change server start script

--- examples/gRPC/scripts/start_servers.sh
+++ examples/gRPC/scripts/start_servers.sh
@@ -1,9 +1,5 @@
 BATCH_SIZE=4
-./run.sh -m resnet50 --batchsize $BATCH_SIZE --deviceid "0" --port 5001&
-./run.sh -m resnet50 --batchsize $BATCH_SIZE --deviceid "1" --port 5002&
-./run.sh -m resnet50 --batchsize $BATCH_SIZE --deviceid "2" --port 5003&
-./run.sh -m resnet50 --batchsize $BATCH_SIZE --deviceid "3" --port 5004&
-./run.sh -m resnet50 --batchsize $BATCH_SIZE --deviceid "4" --port 5005&
-./run.sh -m resnet50 --batchsize $BATCH_SIZE --deviceid "5" --port 5006&
-./run.sh -m resnet50 --batchsize $BATCH_SIZE --deviceid "6" --port 5007&
-./run.sh -m resnet50 --batchsize $BATCH_SIZE --deviceid "7" --port 5008&
+./run.sh -m resnet50 --batchsize $BATCH_SIZE --deviceid "0 1" --port 5001&
+./run.sh -m resnet50 --batchsize $BATCH_SIZE --deviceid "2 3" --port 5002&
+./run.sh -m resnet50 --batchsize $BATCH_SIZE --deviceid "4 5" --port 5003&
+./run.sh -m resnet50 --batchsize $BATCH_SIZE --deviceid "6 7" --port 5004&

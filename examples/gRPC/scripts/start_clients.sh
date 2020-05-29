5df12af17203143d8b8f0ed5fe40124303bb5596
Tom Lou <louyu27@cs.washington.edu>
2020-05-28 18:30:04 -0700

    Use streaming

--- examples/gRPC/scripts/start_clients.sh
+++ examples/gRPC/scripts/start_clients.sh
@@ -1,4 +1,4 @@
-python naive_client.py --batchsize 4 -n 8000 -p 5001&
-python naive_client.py --batchsize 4 -n 8000 -p 5002&
-python naive_client.py --batchsize 4 -n 8000 -p 5003&
-python naive_client.py --batchsize 4 -n 8000 -p 5004&
+python naive_client.py --batchsize 4 -n 8000 -p 5001 --stream&
+python naive_client.py --batchsize 4 -n 8000 -p 5002 --stream&
+python naive_client.py --batchsize 4 -n 8000 -p 5003 --stream&
+python naive_client.py --batchsize 4 -n 8000 -p 5004 --stream&
